56c63b65c568b4bbc052e6a0fcd47b66b4f96bb2
Ahmar Jabbar <87254977+ahmar7@users.noreply.github.com>
2022-07-15 18:44:39 +0500

    Bet update

---
+++ public/js/custom.js
@@ -0,0 +1,9 @@
+
+openLogin = () => {
+    let datamodal = document.getElementById("data_rev")
+    datamodal.style.display = "block"
+}
+hideLogin = () => {
+    let datamodal = document.getElementById("data_rev")
+    datamodal.style.display = "none"
+}
